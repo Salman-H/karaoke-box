9169191d8aa11b6a5f5f4c6008544cd1699719f9
Salman-H <sah517@g.harvard.edu>
2018-10-11 04:27:41 -0400

    feat: Add constructor and getters for Song class

--- src/model/Song.java
+++ src/model/Song.java
@@ -4,5 +4,25 @@ public class Song {
 
     private String title;
     private String artist;
-    private String videUrl;
+    private String videoUrl;
+
+    public Song(String title, String artist, String videoUrl) {
+        this.title = title;
+        this.artist = artist;
+        this.videoUrl = videoUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+
 }
