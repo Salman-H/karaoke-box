499ad6181ddddb811edd88c23dfcd3af3e887192
Salman-H <sah517@g.harvard.edu>
2018-10-11 05:31:49 -0400

    feat: Create class for a song library

---
+++ src/model/SongLibrary.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SongLibrary {
+    private List<Song> songs;
+
+    public SongLibrary() {
+        songs = new ArrayList<Song>();
+    }
+}
