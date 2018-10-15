de13587d87ebb6288e6ad9b2e11d06a91de1628b
Salman-H <sah517@g.harvard.edu>
2018-10-15 12:34:56 -0400

    feat: Add helper method to get user option for machine

--- src/KaraokeMachine.java
+++ src/KaraokeMachine.java
@@ -3,17 +3,33 @@ import model.SongLibrary;
 import java.io.BufferedReader;
 import java.util.HashMap;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Map;
 
 
 public class KaraokeMachine {
     private SongLibrary songLibrary;
-    private BufferedReader userInput;
+    private BufferedReader inputReader;
     private Map<String,String> menu;
 
     public KaraokeMachine(SongLibrary songLibrary) {
         this.songLibrary = songLibrary;
-        userInput = new BufferedReader(new InputStreamReader(System.in));
+        inputReader = new BufferedReader(new InputStreamReader(System.in));
         menu = new HashMap<String,String>();
+        menu.put("add", "Add new song to library");
+        menu.put("quit", "Exit program");
     }
+
+    private String getUserInput() throws IOException {
+        System.out.printf("There are %d songs available. Here are your options: %n",
+                            songLibrary.getSongCount());
+        for (Map.Entry<String,String> option: menu.entrySet()) {
+            System.out.printf("%s - % s", option.getKey(), option.getValue());
+        }
+        System.out.print("What would you like to do?");
+        String userChoice = inputReader.readLine();
+
+        return userChoice.trim().toLowerCase();
+    }
+
 }
