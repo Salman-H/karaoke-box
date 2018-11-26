73da9ec33cbd9eaab8eff00e5639a8b50d94697d
Salman-H <sah517@g.harvard.edu>
2018-11-25 21:19:55 -0500

    refactor: Decrease 'vertical distance' between methods

--- src/KaraokeMachine.java
+++ src/KaraokeMachine.java
@@ -1,3 +1,4 @@
+import model.Artist;
 import model.Song;
 import model.SongLibrary;
 
@@ -9,62 +10,65 @@ import java.util.Map;
 
 
 public class KaraokeMachine {
-    private SongLibrary songLibrary;
-    private BufferedReader inputReader;
-    private Map<String,String> menu;
+   private SongLibrary songLibrary;
+   private BufferedReader inputReader;
+   private Map<String,String> menu;
 
-    public KaraokeMachine(SongLibrary songLibrary) {
-        this.songLibrary = songLibrary;
-        inputReader = new BufferedReader(new InputStreamReader(System.in));
-        menu = new HashMap<String,String>();
-        menu.put("add", "Add new song to library");
-        menu.put("quit", "Exit program");
-    }
+   public KaraokeMachine(SongLibrary songLibrary) {
+      this.songLibrary = songLibrary;
+      inputReader = new BufferedReader(new InputStreamReader(System.in));
+      menu = new HashMap<String,String>();
+      menu.put("add", "Add new song to library");
+      menu.put("quit", "Exit program");
+   }
 
-    private String getUserInput() throws IOException {
-        System.out.printf("There are %d songs available. Here are your options: %n",
-                            songLibrary.getSongCount());
-        for (Map.Entry<String,String> option: menu.entrySet()) {
-            System.out.printf("%s - %s %n", option.getKey(), option.getValue());
-        }
-        System.out.print("What would you like to do?  ");
-        String userChoice = inputReader.readLine();
-
-        return userChoice.trim().toLowerCase();
-    }
-
-    public void run() {
-        String choice = "";
-        do {
-            try {
-                choice = getUserInput();
-                switch (choice) {
-                    case "add":
-                        Song song = promptNewSong();
-                        songLibrary.addSong(song);
-                        System.out.printf("%s added! %n%n", song);
-                        break;
-                    case "quit":
-                        System.out.println("Thanks for singing!");
-                        break;
-                    default:
-                        System.out.printf("Choice not recognized: %s. Try again. %n%n%n", choice);
-                }
-            } catch(IOException ioe) {
-                System.out.println("Problem with input!");
-                ioe.printStackTrace();
+   public void run() {
+      String choice = "";
+      do {
+         try {
+            choice = getUserInput();
+            switch (choice) {
+               case "add":
+                  Song song = promptNewSong();
+                  songLibrary.addSong(song);
+                  System.out.printf("%s added! %n%n", song);
+                  break;
+               case "quit":
+                  System.out.println("Thanks for singing!");
+                  break;
+               default:
+                  System.out.printf("Choice not recognized: %s. " +
+                                    "Try again. %n%n%n", choice);
             }
-        }
-        while (!choice.equalsIgnoreCase("quit"));
-    }
+         }
+         catch(IOException ioe) {
+            System.out.println("Problem with input!");
+            ioe.printStackTrace();
+         }
+      }
+      while (!choice.equalsIgnoreCase("quit"));
+   }
 
-    private Song promptNewSong() throws IOException {
-        System.out.print("Enter the artist's name: ");
-        String artist = inputReader.readLine();
-        System.out.print("Enter the title: ");
-        String title = inputReader.readLine();
-        System.out.print("Enter the video URL: ");
-        String videoURL = inputReader.readLine();
-        return new Song(title, artist, videoURL);
-    }
+   private String getUserInput() throws IOException {
+      System.out.printf("There are %d songs available. " +
+                        "Here are your options: %n",
+                        songLibrary.getSongCount());
+      for (Map.Entry<String,String> option: menu.entrySet()) {
+         System.out.printf("%s - %s %n", option.getKey(), option.getValue());
+      }
+      System.out.print("What would you like to do?  ");
+      String userChoice = inputReader.readLine();
+      return userChoice.trim().toLowerCase();
+   }
+
+   private Song promptNewSong() throws IOException {
+      System.out.print("Enter the artist's name: ");
+      String artist = inputReader.readLine();
+      System.out.print("Enter the title: ");
+      String title = inputReader.readLine();
+      System.out.print("Enter the video URL: ");
+      String videoURL = inputReader.readLine();
+      return new Song(title, new Artist(artist), videoURL);
+   }
+
 }
