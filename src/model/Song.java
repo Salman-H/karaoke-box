c831a8f5c19191a1dd4abd1181d48336c2137ce5
Salman-H <sah517@g.harvard.edu>
2018-11-25 20:47:19 -0500

    chore: Decrease indent space to preserve horizontal space

--- src/model/Song.java
+++ src/model/Song.java
@@ -1,31 +1,30 @@
 package model;
 
 public class Song {
+   private String title;
+   private Artist artist;
+   private String videoUrl;
 
-    private String title;
-    private Artist artist;
-    private String videoUrl;
+   public Song(String title, Artist artist, String videoUrl) {
+      this.title = title;
+      this.artist = artist;
+      this.videoUrl = videoUrl;
+   }
 
-    public Song(String title, Artist artist, String videoUrl) {
-        this.title = title;
-        this.artist = artist;
-        this.videoUrl = videoUrl;
-    }
+   public String getTitle() {
+      return title;
+   }
 
-    public String getTitle() {
-        return title;
-    }
+   public Artist getArtist() {
+      return artist;
+   }
 
-    public Artist getArtist() {
-        return artist;
-    }
+   public String getVideoUrl() {
+      return videoUrl;
+   }
 
-    public String getVideoUrl() {
-        return videoUrl;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Song: %s by %s", title, artist.getName());
-    }
+   @Override
+   public String toString() {
+      return String.format("Song: %s by %s", title, artist.getName());
+   }
 }
--- src/model/SongLibrary.java
+++ src/model/SongLibrary.java
@@ -4,33 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SongLibrary {
-    private List<Song> songs;
-    private List<Artist> artists;
+   private List<Song> songs;
+   private List<Artist> artists;
 
-    public SongLibrary() {
-        songs = new ArrayList<>();
-        artists = new ArrayList<>();
-    }
+   public SongLibrary() {
+      songs = new ArrayList<>();
+      artists = new ArrayList<>();
+   }
 
-    public void addSong(Song song) {
-        songs.add(song);
-        song.getArtist().addSong(song);
-        artists.add(song.getArtist());
-    }
+   public void addSong(Song song) {
+      songs.add(song);
+      song.getArtist().addSong(song);
+      artists.add(song.getArtist());
+   }
 
-    public int getSongCount() {
-        return songs.size();
-    }
+   public int getSongCount() {
+      return songs.size();
+   }
 
-    public List<Artist> getArtists() {
-        return artists;
-    }
+   public List<Artist> getArtists() {
+      return artists;
+   }
 
-    public List<Song> getSongsForArtist(Artist artist) {
-        return artist.getSongs();
-    }
+   public List<Song> getSongsForArtist(Artist artist) {
+      return artist.getSongs();
+   }
 
-    public void displaySongsByArtist() {
-        artists.forEach(System.out::println);
-    }
+   public void displaySongsByArtist() {
+      artists.forEach(System.out::println);
+   }
 }
