b063934967fe2225eee9f413e4a257d18c1d538d
Salman-H <sah517@g.harvard.edu>
2018-11-26 06:10:58 -0500

    feat: Display songs by artist and override equals

--- src/model/Artist.java
+++ src/model/Artist.java
@@ -25,7 +25,34 @@ public class Artist {
    }
 
    @Override
+   public boolean equals(Object obj) {
+      if (this == obj) return true;
+      if (obj == null) return false;
+      if (obj instanceof Artist) {
+         Artist other = (Artist) obj;
+         return other.getName().equalsIgnoreCase(this.name);
+      }
+      return false;
+   }
+
+   public void printInfo() {
+      System.out.printf("%n%s | ", name.toUpperCase());
+      if (!songs.isEmpty()) {
+         System.out.print("Available songs: \n\n");
+         int count = 1;
+         for (Song song: songs) {
+            System.out.printf("%d)  %s %n%n",
+                    count, song.getTitle().toUpperCase());
+            count++;
+         }
+      }
+      else {
+         System.out.println("No songs added yet.");
+      }
+   }
+
+   @Override
    public String toString() {
-        return String.format("artist: %s %n songs: %s %n", name, songs.toString());
-    }
+      return String.format("artist: %s %n songs: %s %n", name, songs.toString());
+   }
 }
