ee35f4ce626ed8920fc552bda4bbf3d711a11e67
Salman-H <sah517@g.harvard.edu>
2018-11-25 19:56:52 -0500

    feat: Associate aritst with song library A list of artists is added as an attribute so it can be updated each time a new song is added. The addSong method is modified to also add that song to the artist's list of songs in addition to the song library. Methods are added to access artists in the library as well as songs by a given artist.

--- src/model/SongLibrary.java
+++ src/model/SongLibrary.java
@@ -5,16 +5,32 @@ import java.util.List;
 
 public class SongLibrary {
     private List<Song> songs;
+    private List<Artist> artists;
 
     public SongLibrary() {
-        songs = new ArrayList<Song>();
+        songs = new ArrayList<>();
+        artists = new ArrayList<>();
     }
 
     public void addSong(Song song) {
         songs.add(song);
+        song.getArtist().addSong(song);
+        artists.add(song.getArtist());
     }
 
     public int getSongCount() {
         return songs.size();
+    }
+
+    public List<Artist> getArtists() {
+        return artists;
+    }
+
+    public List<Song> getSongsForArtist(Artist artist) {
+        return artist.getSongs();
+    }
+
+    public void displaySongsByArtist() {
+        artists.forEach(System.out::println);
     }
 }
